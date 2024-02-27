6e9519404401f0cc85fda64fcdf04b24228decb6
Roei Rom <roeirom6@gmail.com>
2024-02-28 00:42:21 +0200

    add body logging

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import { Context, StructuredReturn } from 'faas-js-runtime';
 
 export const handle = async (context: Context, body: any): Promise<StructuredReturn> => {
+    context.log.info(body);
     return {
         statusCode: StatusCodes.OK,
         body
